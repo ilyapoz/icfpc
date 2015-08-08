bc02b77ee1b292178808679e5a713b1c83d761cf
Boris Yangel <hr0nix@hr0nix-osx2.local>
2015-08-08 17:10:35 +0300

    Score computations.

--- simple.py
+++ simple.py
@@ -17,14 +17,14 @@ class Board:
             self.board[cell_cfg['x']][cell_cfg['y']] = 1
 
     def occupied(self, pos):
-        return self.board[pos[0]][pos[1]] == 0
+        return self.board[pos[0]][pos[1]] == 1
 
     def line_occupied(self, y):
-        return all(self.occupied(x, y) for x in range(0, self.width))
+        return all(self.occupied((x, y)) for x in range(0, self.width))
 
-    def flush_line(self, y):
+    def clear_line(self, y):
         # Move one line down
-        for yy in range(y).reverse():
+        for yy in range(y - 1, -1, -1):
             for x in range(0, self.width):
                 self.board[x][yy + 1] = self.board[x][yy]
         for x in range(0, self.width):
@@ -34,25 +34,30 @@ class Board:
         self.board[pos[0]][pos[1]] = 1
 
         y = self.height - 1
+        lines_cleared = 0
         while y >= 0:
-            line_occupied = self.line_occupied(y)
-            if line_occupied:
-                self.flush_line(y)
+            occupied = self.line_occupied(y)
+            if occupied:
+                print 'Line cleared!'
+                lines_cleared += 1
+                self.clear_line(y)
             else:
                 # Go to the next line
-                y += 1
+                y -= 1
+
+        return lines_cleared
 
     def find_available_locks(self, pos):
         visited_positions = set()
         pos_to_lock = {}
         self.generate_positions_dfs(pos, '', visited_positions, pos_to_lock)
-        return pos_to_lock.values
+        return pos_to_lock.values()
 
     def east(self, pos):
-        return pos[0] - 1, pos[1]
+        return pos[0] + 1, pos[1]
 
     def west(self, pos):
-        return pos[0] + 1, pos[1]
+        return pos[0] - 1, pos[1]
 
     def south_east(self, pos):
         if pos[1] % 2 == 0:
@@ -67,7 +72,7 @@ class Board:
             return pos[0], pos[1] + 1
 
     def get_start_pos(self):
-        return self.width / 2, 0
+        return (self.width - 1) / 2, 0
 
     def add_lock(self, pos, moves, pos_to_lock):
         if pos not in pos_to_lock:
@@ -78,16 +83,25 @@ class Board:
             return
         visited_positions.add(pos)
 
-        for move_info in [(self.east, ''), (self.west, ''), (self.south_east, ''), (self.south_west, '')]:
+        #for move_info in [(self.east, 'b'), (self.west, 'p'), (self.south_east, 'm'), (self.south_west, 'g')]:
+        for move_info in [(self.east, 'e'), (self.west, '!'), (self.south_east, 'o'), (self.south_west, 'a')]:
+        #for move_info in [(self.east, 'E'), (self.west, 'W'), (self.south_east, 'SE'), (self.south_west, 'SW')]:
             new_pos = move_info[0](pos)
             if new_pos[0] < 0 or new_pos[0] >= self.width or new_pos[1] >= self.height or self.occupied(new_pos):
                 self.add_lock(pos, moves + move_info[1], pos_to_lock)
             else:
                 self.generate_positions_dfs(new_pos, moves + move_info[1], visited_positions, pos_to_lock)
 
+
+def compute_points(unit_size, lines_cleared, lines_cleared_prev):
+    points = unit_size + 100 * (1 + lines_cleared) * lines_cleared / 2
+    line_bonus = (lines_cleared_prev - 1) * points / 10 if lines_cleared_prev > 1 else 0
+    return points + line_bonus
+
+
 def main():
-    if len(sys.argv) != 2:
-        print 'Usage: %s <file.json>' % sys.argv[0]
+    if len(sys.argv) != 3:
+        print 'Usage: %s <input.json> <output.json>' % sys.argv[0]
         sys.exit(1)
 
     config = json.loads(open(sys.argv[1], 'r').read())
@@ -95,18 +109,27 @@ def main():
     assert len(config['units'][0]['members']) == 1
     assert len(config['sourceSeeds']) == 1 and config['sourceSeeds'][0] == 0
 
-    solution = [{'problemId': '1', 'seed': config['sourceSeeds'], 'tag': 'simpleShit', 'solution': ''}]
+    solution = [{'problemId': config['id'], 'seed': config['sourceSeeds'][0], 'tag': 'simpleShit2', 'solution': ''}]
 
     board = Board(config)
+    lines_cleared_prev = 0
+    total_points = 0
     for i in range(config['sourceLength']):
         piece_pos = board.get_start_pos()
         assert not board.occupied(piece_pos)
         available_locks = board.find_available_locks(piece_pos)
-        best_lock = max(available_locks, key=lambda l: l.pos.y)
-        solution['solution'] += best_lock.moves
-        board.fill(best_lock.pos)
+        best_lock = max(available_locks, key=lambda l: l.pos[1])
+        solution[0]['solution'] += best_lock.moves
+        lines_cleared = board.fill(best_lock.pos)
+        move_points = compute_points(1, lines_cleared, lines_cleared_prev)
+        print 'Piece %d: %d points' % (i, move_points)
+        lines_cleared_prev = lines_cleared
+        total_points += move_points
 
-    print json.dumps(solution)
+    print 'Total points: %d' % total_points
+
+    with open(sys.argv[2], 'w') as output_file:
+        output_file.write(json.dumps(solution))
 
 if __name__ == '__main__':
     main()
