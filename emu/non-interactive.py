f93bb4fe09add3ad127650047b108643f2e6c436
Alexander Yuriev <alexander.yuriev@gmail.com>
2015-08-10 00:44:57 +0300

    Non-interactive

--- emu/non-interactive.py
+++ emu/non-interactive.py
@@ -9,6 +9,15 @@ import json
 import curses
 import logging
 
+def func(game, line_score, phrase_score):
+    board = game.board()
+    res = 0
+
+    for x in xrange(board.width):
+        for y in xrange(board.height):
+            if board.field[x, y] != 0: res += y
+
+    return res + line_score * 100
 
 def play(game, screen, game_index, game_count):
     button_to_phrase = [(ord('1') + i, phrases.all[i]) for i in xrange(len(phrases.all))]
@@ -17,7 +26,6 @@ def play(game, screen, game_index, game_count):
 
     last_res = ''
     can_quit = False
-    lock_searcher = locker.LockSearcher(game)
     seq = ''
 
     while not can_quit:
@@ -25,21 +33,57 @@ def play(game, screen, game_index, game_count):
         phrase_score = game.phrase_score()
         total_score = line_score + phrase_score
 
-        #screen.addstr(1, 5, 'Game %d out of %d, unit %d out of %d' % \
-        #              (game_index + 1, game_count, game.current_state().unit_index + 1, len(game.units)))
-        #screen.addstr(2, 5, 'Score: %d + %d = %d' % (line_score, phrase_score, total_score))
-        #screen.addstr(3, 5, 'Controls: W to go west, E to go east, A to go south west, D to go south east,')
-        #screen.addstr(4, 5, '          Q to turn ccw, R to turn clockwise, Z to cancel move, 0 to stop the current game.')
-        #screen.addstr(5, 5, '          %s' % phrase_legend)
-        #screen.addstr(6, 5, '          %s' % str(last_res))
+        screen.addstr(1, 5, 'Game %d out of %d, unit %d out of %d' % \
+                      (game_index + 1, game_count, game.current_state().unit_index + 1, len(game.units)))
+        screen.addstr(2, 5, 'Score: %d + %d = %d' % (line_score, phrase_score, total_score))
+        screen.addstr(3, 5, 'Controls: W to go west, E to go east, A to go south west, D to go south east,')
+        screen.addstr(4, 5, '          Q to turn ccw, R to turn clockwise, Z to cancel move, 0 to stop the current game.')
+        screen.addstr(5, 5, '          %s' % phrase_legend)
+        screen.addstr(6, 5, '          %s' % str(last_res))
 
-        #screen.addstr(8, 0, game.board().get_field_str(game.cur_unit_pos(), ext=0))
+        screen.addstr(8, 0, game.board().get_field_str(game.cur_unit_pos(), ext=0))
 
 
-        #screen.refresh()
+        screen.refresh()
 
         if seq == '':
-            seq = lock_searcher.find_lock_states()
+            (lock_state, back_move) = locker.LockSearcher(game).find_lock_states()
+
+            cur_pos = game.current_state().unit_pos
+            cur_unit = cur_pos.unit
+
+            best_lock_state, best_lock_score = None, 0
+
+            for state, letter in lock_state.iteritems():
+                line_score = -game.line_score()
+                phrase_score = 0
+
+                undo_times = 1
+                pos_before_lock = emu.Position(cur_unit, state[0], state[1])
+
+                if ((cur_pos.pivot, cur_pos.rotation) !=
+                    (pos_before_lock.pivot, pos_before_lock.rotation)):
+                    game.commit_pos(pos_before_lock, '?')
+                    undo_times += 1
+
+                lock_pos = pos_before_lock.apply_char(letter)
+                game.commit_pos(lock_pos, letter)
+
+                line_score += game.line_score()
+                position_score = func(game, line_score, phrase_score)
+
+                game.undo(undo_times)
+
+                if (   best_lock_state is None
+                    or best_lock_score < position_score):
+                    best_lock_state = state
+                    best_lock_score = position_score
+
+            cur_state = (best_lock_state, lock_state[best_lock_state])
+
+            while cur_state is not None:
+                seq = cur_state[1] + seq
+                cur_state = back_move.get(cur_state[0])
 
         key = ord('w')
 
