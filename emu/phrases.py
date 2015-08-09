83584228a051409f4df4820df0fcf65a6783321d
hr0nix <boris.jangel@gmail.com>
2015-08-09 20:30:17 +0300

    New phrase added.

--- emu/phrases.py
+++ emu/phrases.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python
 
-all = ["Yuggoth", "Ia! Ia!", "R'lyeh", "Ei!", "Cthulhu Fhtagn!"]
+all = ["Yuggoth", "Ia! Ia!", "R'lyeh", "Ei!", "Cthulhu Fhtagn!", "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn."]
